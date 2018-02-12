4ab6335fd152bce9e155d2c941a7bb05bc25269c
Ben Tobias <benedict.tobias@gmail.com>
2018-02-12 00:06:54 +0000

    Finished Tutorial part 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Question, Choice
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.views import generic
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	# Faster way using render(request, <template_path>, context)
-	## template = loader.get_template('polls/index.html')
-	context = {
-		'latest_question_list': latest_question_list
-	}
-	return render(request, 'polls/index.html', context)
+from .models import Question, Choice
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
+	def get_queryset(self):
+		#Return the last five published questions.
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
@@ -26,8 +27,7 @@ def vote(request, question_id):
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
 		# Redisplay the question voting form.
-		return render(request, 'polls/detail.html', {'question': question, 'error_message': "You did not select a choice",
-			})
+		return render(request, 'polls/detail.html', {'question': question, 'error_message': "You did not select a choice",})
 	else:
 		selected_choice.votes += 1
 		selected_choice.save()
